b3eb466f1f60c16aed978ce1b8dfcb0cd03af74e
npsaunders <neilpsaunders@gmail.com>
2022-09-27 10:56:49 -0400

    add compoonents and pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//Import SCSS file to be source of styling
+import './styles.scss';
+//Import Router
+import { BrowserRouter as Router } from 'react-router-dom';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router >
 );
 
 // If you want to start measuring performance in your app, pass a function
